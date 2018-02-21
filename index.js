23b486fe24b70bacd86840436372c5c2b0e185a9
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2018-02-21 17:32:38 +0700

    Redirect root to test.html

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ express = require('express');
 const app = express();
 
 app.use(express.static('static'))
+
+app.get('/', function(req, res) {
+    res.redirect('/test.html');
+});
+
 app.use(function(req, res) {
     res.status(404);
     res.send('404 File not found.');
     res.end();
-});
-
-app.get('/', function(req, res) {
-    res.redirect('/test.html');
 });
 
 app.listen(APP_PORT, function() {
